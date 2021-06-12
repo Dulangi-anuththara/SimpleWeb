ae85940ce48e3cfe523e9af96538a311d07924c3
Dulangi-anuththara <anuththaragamage104@gmail.com>
2021-06-12 13:45:57 +0530

    connection establishment

--- webserver.c
+++ webserver.c
@@ -1,22 +1,86 @@
-#include<stdlib.h> 
-#include <stdio.h>
-#include <winsock2.h> // header file for socket in windows
+#include<io.h>
+#include<stdio.h>
+#include<winsock2.h>
 
-#pragma comment(lib,"ws2_32.lib") // library file to be linked to be able to use winsock functions
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
 
-int main(int argc, char *argv[]){
-	
+int main(int argc , char *argv[])
+{
 	WSADATA wsa;
-	
-	printf("\nInitialising Winsock....");
-	
-	if(WSAStartup(MAKEWORD(2,2),&wsa) != 0){
-//		printf("Failed. Error Code: %d", WSAGetLastError());
-//		return 1;
+	SOCKET s , new_socket;
+	struct sockaddr_in server , client;
+	int c;
+	char *message;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+	{
+		printf("Failed. Error Code : %d",WSAGetLastError());
+		return 1;
 	}
+	
+	printf("Initialised.\n");
+	
+	//Create a socket
+	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d" , WSAGetLastError());
+	}
+
+	printf("Socket created.\n");
+	
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	
+	//Bind
+	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d" , WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	
+	puts("Bind done");
+
+	//Listen to incoming connections
+	listen(s , 3);
+	
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	
+	c = sizeof(struct sockaddr_in);
+		new_socket = accept(s , (struct sockaddr *)&client, &c);
+	if (new_socket == INVALID_SOCKET)
+	{
+		printf("accept failed with error code : %d" , WSAGetLastError());
+	}
+	
+	puts("Connection accepted");
+	
+	//Reply to client
+	message = "Hello Client , I have received your connection. But I have to go now, bye\n";
+	send(new_socket , message , strlen(message) , 0);
+	
+	getchar();
+	
+//	while( (new_socket = accept(s , (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
+//	{
+//		puts("Connection accepted");
+//		
+//		//Reply to the client
+//		message = "Hello Client , I have received your connection. But I have to go now, bye\n";
+//		send(new_socket , message , strlen(message) , 0);
+//	}
 //	
-//	printf("Initialised");
+//	if (new_socket == INVALID_SOCKET)
+//	{
+//		printf("accept failed with error code : %d" , WSAGetLastError());
+//		return 1;
+//	}
+
+	closesocket(s);
+	WSACleanup();
 	
 	return 0;
-
 }
